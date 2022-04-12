3970f1622eb2a70de3c4cc0d1086e9fb344439c7
castrintt <igordc38@gmail.com>
2022-04-12 00:22:57 -0300

    resolução professor listapt 2

--- colecoes/exercicios-listaspt2.py
+++ colecoes/exercicios-listaspt2.py
@@ -8,6 +8,8 @@
 #todas as informações de um jogo devem estar no mesmo indice nas listas. Criar um menu interativo com as seguintes opções para o usuario: registrar venda, Compra de estoque, resumo da loja, sair. Ao sair indicar que o caixa está fechado. O usuario deve controlar o sistema da loja, registrando as vendas e as compras de estoque, sem esquecer de alterar os valores da lista de quantidade
 
 
+
+
 print('============Bem vindo a loja de games===============')
 
 
@@ -105,5 +107,52 @@ print(f'\nSaldo final loja: {saldoLoja}, jogos disponiveis: {jogosDisponiveis}')
 
 
 
+
+
+
 #resolução professor
 
+
+
+jogos = ['Gow','MineCraft','GTA','Sonic','FIFA'] # jogos disponiveis para venda
+precosVenda = [210.0,2.99,150.00,1.80,125.00] # preços de venda
+precosCompraEstoque = [105.00,1.50,75.00,0.90,62.50] # preços para o dono da loja comprar da fabrica
+quantidade = [3,0,2,5,1] #quantidade de jogos disponiveis
+vendas = [] # Vendas registradas
+compraDeEstoque = [] # compras de estoque registradas
+
+
+op = 0
+
+while op != 4:
+    print('1 - registrar venda \n2 - compra de estoque \n3- resumo da loja \n4- sair')
+    op = int(input('Opção: '))
+    
+    if op == 1:
+        jogoVendido = input('Jogo vendido: ')
+        indiceJogosVendidos = jogos.index(jogoVendido)
+        quantidadeVendida = int(input('Quantidade vendida: '))
+        if quantidadeVendida <= quantidade[indiceJogosVendidos]:
+            print(f'\n{jogoVendido} Vendido!')
+            quantidade[indiceJogosVendidos] -= quantidadeVendida
+            vendas.append(precosVenda[indiceJogosVendidos] * quantidadeVendida)
+        else:
+            print('\nNão há essa quantidade disponivel no estoque')
+    elif op == 2:
+        jogoComprado = input('Jogo comprado: ')
+        quantidadeComprada = int(input('Quantidade comprada: '))
+        print(f'{jogoComprado} comprado!')
+        indiceJogosComprados = jogos.index(jogoComprado)
+        quantidade[indiceJogosComprados] += quantidadeComprada
+        compraDeEstoque.append(precosCompraEstoque[indiceJogosComprados] * quantidadeComprada)
+    elif op == 3:
+        print('\n\nQuantidade de jogos em estoque')
+        for jogo in jogos:
+            print(f'{jogo}: {quantidade[jogos.index(jogo)]}')
+        print(f'Lucros: R${sum(vendas)}')
+        print(f'Despesas: R${sum(compraDeEstoque)}')
+        print(f'Caixa final: R${sum(vendas) - sum(compraDeEstoque)}')
+    else:
+        print('Saindo!')
+        break
+     
