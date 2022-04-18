3aad3a32b8495195b0153a363c285adea8a2de98
castrintt <igordc38@gmail.com>
2022-04-17 22:56:00 -0300

    somente enunciado de exercicio para funções com parametros

---
+++ funcoes/exercicio-funcao-com-parametro.py
@@ -0,0 +1,19 @@
+# 1) criar uma função recursiva ( que retorna ela mesma ) oara armazenar N termos da sequência de Fibonacci em uma lista. N é definido pelo usuario. Ao encontra os termos, imprimir a lista e finalizar a função
+
+
+# 2) criar 5 funções: uma para um cadastro, outra para realizar o login, outra para mudar a senha, outra para realizar logout e ainda uma para definir qual opção o usuario deseja escolher. Utilize um loop while para sair do sistema apenas se o usuario desejar ( criar a opção 'sair').
+# atente-se as regras:
+
+# Só é possivel realizar um cadastro se não houver nenhum anterior.
+
+# Só é possivel realizar login se houver um cadastro.
+
+# Só é possivel realizar login se o usuario informar corretamente username e senha.
+
+# Só é possivel alterar a senha se o usuario estiver logado.
+
+# Só é possivel alterar a senha se o usuario informar corretamente a senha atual
+
+# Só é possivel realizar logout se o usuario estiver logado.
+
+
