e27a46296ddfc2c1f997070bfb2d38d1bacd98bb
castrintt <igordc38@gmail.com>
2022-04-18 23:15:44 -0300

    funcoes com parametros exercicios

--- funcoes/exercicio-funcao-com-parametro.py
+++ funcoes/exercicio-funcao-com-parametro.py
@@ -1,4 +1,4 @@
-# 1) criar uma função recursiva ( que retorna ela mesma ) oara armazenar N termos da sequência de Fibonacci em uma lista. N é definido pelo usuario. Ao encontra os termos, imprimir a lista e finalizar a função
+# 1) criar uma função recursiva ( que retorna ela mesma ) para armazenar N termos da sequência de Fibonacci em uma lista. N é definido pelo usuario. Ao encontrar os termos, imprimir a lista e finalizar a função
 
 
 # 2) criar 5 funções: uma para um cadastro, outra para realizar o login, outra para mudar a senha, outra para realizar logout e ainda uma para definir qual opção o usuario deseja escolher. Utilize um loop while para sair do sistema apenas se o usuario desejar ( criar a opção 'sair').
@@ -16,4 +16,18 @@
 
 # Só é possivel realizar logout se o usuario estiver logado.
 
+import pdb
 
+
+#  resolução exercicio 1
+
+#fibonacci
+
+def fibonacci():
+    escolha = int(input('Quantos termos deseja mostrar? '))
+    #pdb.set_trace()
+    listaFibo = [1 + (n + (n-1)) for n in range(1,(escolha + 1))]
+    
+    return listaFibo
+
+print(fibonacci())
