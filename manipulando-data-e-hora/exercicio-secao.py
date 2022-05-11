7abfb51a1471c11b07c78e6bb450216aa4b8cb60
castrintt <igordc38@gmail.com>
2022-05-11 16:21:18 -0300

    alterações

--- manipulando-data-e-hora/exercicio-secao.py
+++ manipulando-data-e-hora/exercicio-secao.py
@@ -5,32 +5,32 @@
 # minha resolução!
 
 
-# from random import choice as ch
-# import datetime
+from random import choice as ch
+import datetime
 
-# listaDePosts = ['Comer verdura faz bem','Pratique um esporte','Vamos voce consegue, estude!']
+listaDePosts = ['Comer verdura faz bem','Pratique um esporte','Vamos voce consegue, estude!']
 
-# dicionarioDias = {
-#     'segunda':[0,7,14,21,28],
-#     'terça':[1,8,15,22,29],
-#     'quarta':[2,9,16,23,30],
-#     'quinta':[3,10,17,24,31],
-#     'sexta':[4,11,18,25],
-#     'sabado':[5,12,19,26],
-#     'domingo':[6,13,20,27]
-# }
+dicionarioDias = {
+    'segunda':[0,7,14,21,28],
+    'terça':[1,8,15,22,29],
+    'quarta':[2,9,16,23,30],
+    'quinta':[3,10,17,24,31],
+    'sexta':[4,11,18,25],
+    'sabado':[5,12,19,26],
+    'domingo':[6,13,20,27]
+}
 
 
-# for day in range(0,31):
-#     # breakpoint()
-#     if day in dicionarioDias['segunda']:
-#         print(f'Hoje é segunda pessoal! --> mensagem do dia --> {ch(listaDePosts)}')
-#     elif day in dicionarioDias['quarta']:
-#         print(f'Hoje é quarta pessoal! --> mensagem do dia --> {ch(listaDePosts)}')
-#     elif day in dicionarioDias['sexta']:
-#         print(f'Hoje é sexta pessoal! --> mensagem do dia --> {ch(listaDePosts)}')
-#     else:
-#         pass
+for day in range(0,31):
+    # breakpoint()
+    if day in dicionarioDias['segunda']:
+        print(f'Hoje é segunda pessoal! --> mensagem do dia --> {ch(listaDePosts)}')
+    elif day in dicionarioDias['quarta']:
+        print(f'Hoje é quarta pessoal! --> mensagem do dia --> {ch(listaDePosts)}')
+    elif day in dicionarioDias['sexta']:
+        print(f'Hoje é sexta pessoal! --> mensagem do dia --> {ch(listaDePosts)}')
+    else:
+        pass
 
 
 # resolução professor
