3a8b1e1ff474cc78ed7052a7bc93f63e1d70dd5d
castrintt <igordc38@gmail.com>
2022-05-16 23:45:25 -0300

    exercicio unittest(sem resolução)

---
+++ testes-em-python/exercicio-unittest.py
@@ -0,0 +1,10 @@
+# 1 ) Rafael deseja entrar em uma onda fitness, para isso colocou varias metas como, acordar cedo, definir um tempo de exercicio, definir dias de descanso e qual esporte iria praticar. Para isso aplique o metodo TDD para criar e testar as proximas tres funções
+
+# a ) acordar_cedo: Essa função recebe a hora que ele acordou como parametro e testa se ele acordou após as 6 hrs, caso afirmativo retorne 'tente novamente amanhã', caso contrario 'você é um guerreiro'
+
+# b ) tempo_exercicio: essa função recebe a a quantidade de horas exercitadas e o peso (kg) de rafael, caso o tempo de exercicio seja inferior a 2 hrs, utilize pass, caso contrario, reduza 1 kg do peso e retorne o valor
+
+# c ) tem_exercicio: função que reconhece se é um dia de descanso ou não, recebe como parametro o esporte que ira praticar no dia, caso receba a string 'descanso' retorna false (para mostrar que hoje não é dia de praticar um exercicio) caso contrario retorna true
+
+# Nota : Cada aluno podera desenvolver um conjunto de testes diferente um do outro, mas sera aplicada uma das possiveis soluções abaixo.
+
